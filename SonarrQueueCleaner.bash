b2682df87568868978e0746c5baba84b3ba67fd6
nullip <107959430+nullip@users.noreply.github.com>
2023-01-06 13:35:04 -0600

    Update SonarrQueueCleaner.bash

--- SonarrQueueCleaner.bash
+++ SonarrQueueCleaner.bash
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 # Cron this script to run periodically.  Recommended to run this every 4 hours to 1 day.
-# Need to have python packages xq, jq, and yq.
-pip install xq jq yq
+# Need to have python packages yq
 
-arrRoot="/mnt/user/appdata/..." #Replace with whatever your root is for appdata for your instance.
-ipaddress="192.168.1.x" #insert your local docker sonarr IP address
+pip install yq 
+
+scriptVersion="1.0.003"
+arrRoot="/mnt/user/appdata/x" #Replace with whatever your root is for appdata for your instance.
+ipaddress="x" #insert your local docker sonarr IP address
 
 if [ -z "$arrUrl" ] || [ -z "$arrApiKey" ]; then
   arrUrlBase="$(cat ${arrRoot}/config.xml | xq | jq -r .Config.UrlBase)"
@@ -19,20 +21,21 @@ if [ -z "$arrUrl" ] || [ -z "$arrApiKey" ]; then
   arrUrl="http://${ipaddress}:${arrPort}${arrUrlBase}"
 fi
 
+log () {
+  m_time=`date "+%F %T"`
+  echo $m_time" :: QueueCleaner :: $scriptVersion :: "$1
+}
+
 # auto-clean up log file to reduce space usage
 if [ -f "${arrRoot}/logs/QueueCleaner.txt" ]; then
 	find ${arrRoot}/logs -type f -name "QueueCleaner.txt" -size +1024k -delete
 fi
 
-exec &>> "${arrRoot}/logs/QueueCleaner.txt"
+touch "${arrRoot}/logs/QueueCleaner.txt"
 chmod 666 "${arrRoot}/logs/QueueCleaner.txt"
+exec &> >(tee -a "${arrRoot}/logs/QueueCleaner.txt")
 
-log () {
-  m_time=`date "+%F %T"`
-  echo $m_time" :: QueueCleaner :: "$1
-}
-
-arrQueueData="$(curl -s "$arrUrl/api/v3/queue?page=1&pagesize=1000000000&sortDirection=descending&sortKey=progress&includeUnknownSeriesItems=true&apikey=${arrApiKey}" | jq -r .records[])"
+arrQueueData="$(curl -s "$arrUrl/api/v3/queue?page=1&pagesize=200&sortDirection=descending&sortKey=progress&includeUnknownSeriesItems=true&apikey=${arrApiKey}" | jq -r .records[])"
 arrQueueIds=$(echo "$arrQueueData" | jq -r 'select(.status=="completed") | select(.trackedDownloadStatus=="warning") | .id')
 arrQueueIdsCount=$(echo "$arrQueueData" | jq -r 'select(.status=="completed") | select(.trackedDownloadStatus=="warning") | .id' | wc -l)
 if [ $arrQueueIdsCount -eq 0 ]; then
@@ -46,7 +49,7 @@ else
   done
 fi
 
-arrQueueData="$(curl -s "$arrUrl/api/v3/queue?page=1&pagesize=1000000000&sortDirection=descending&sortKey=progress&includeUnknownSeriesItems=true&apikey=${arrApiKey}" | jq -r .records[])"
+arrQueueData="$(curl -s "$arrUrl/api/v3/queue?page=1&pagesize=200&sortDirection=descending&sortKey=progress&includeUnknownSeriesItems=true&apikey=${arrApiKey}" | jq -r .records[])"
 arrQueueIds=$(echo "$arrQueueData" | jq -r 'select(.status=="failed") | select(.trackedDownloadStatus=="warning") | .id')
 arrQueueIdsCount=$(echo "$arrQueueData" | jq -r 'select(.status=="failed") | select(.trackedDownloadStatus=="warning") | .id' | wc -l)
 if [ $arrQueueIdsCount -eq 0 ]; then
